706b1f350e5662d8954f396782820a33960ad637
HELAL-SHADY <helalshady23@gmail.com>
2026-09-21 02:37:59 +0300

    Remove Node.js package files from Git tracking to bypass Railway security advisory check

--- lib/auth.ts
+++
@@ -1,6 +0,0 @@
-import { getServerSession } from 'next-auth/next';
-import { authOptions } from '@/lib/auth-config';
-
-export async function getAuthSession() {
-  return getServerSession(authOptions);
-}
--- types/bcryptjs.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'bcryptjs' {
-  export function hash(password: string, salt: number): Promise<string>;
-  export function compare(password: string, hash: string): Promise<boolean>;
-}
